21b5a06db4b1727261da827f3b2be1773bc6ef8d
therladbsgh <therladbsgh@gmail.com>
2015-12-20 15:08:34 +0800

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+
+	def index
+		@new_post = Post.new
+		@all_posts = Post.order(created_at: :desc).all
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to root_url
+		else
+			render root
+		end
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:comment)
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threadly::Application.config.secret_key_base = '2c607b82c78dfe776bc84abb0922eb7f44e18da8c1144455eb9dfbf548094aa44ab9669f4eaaea92184fffdb85bbda3d97d5af66a0275f2c6d08ce866c9c79ef'
